0ef8e143bf8383aa84f92c74d1091938c3578234
Wamea <30634582+Wamezach@users.noreply.github.com>
2025-10-06 06:50:37 +0800

    Update lists.js

--- api/lists.js
+++ api/lists.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-// Fast cover fetch from Jikan API (MyAnimeList)
+// Get cover image from Jikan (MyAnimeList) by manga title
 async function getCoverFromJikan(title) {
   try {
     const res = await axios.get('https://api.jikan.moe/v4/manga', { params: { q: title, limit: 1 } });
