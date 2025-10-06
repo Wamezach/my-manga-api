2a16d619435aec82f615cb385857eb8afcfb5a20
Wamea <30634582+Wamezach@users.noreply.github.com>
2025-10-06 08:51:43 +0800

    Update proxy-cover.js

--- api/proxy-cover.js
+++ api/proxy-cover.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  const { baseUrl, mode, hash, filename } = req.query;
-  if(!baseUrl || !mode || !hash || !filename) {
-    return res.status(400).send('Missing required image parameters');
-  }
-  const imageUrl = `${baseUrl}/${mode}/${hash}/${filename}`;
-  try {
-    const response = await axios.get(imageUrl, { responseType: 'stream' });
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', response.headers['content-type']);
-    response.data.pipe(res);
-  } catch (err) {
-    res.status(404).send('Chapter page not found');
-  }
+    const { id, filename } = req.query;
+    if (!id || !filename) {
+        return res.status(400).send('Missing id or filename');
+    }
+
+    const imageUrl = `https://uploads.mangadex.org/covers/${id}/${filename}`;
+
+    try {
+        const response = await axios.get(imageUrl, { responseType: 'stream' });
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Content-Type', response.headers['content-type']);
+        response.data.pipe(res);
+    } catch (err) {
+        res.status(404).send('Cover not found');
+    }
 };
