197e994e258bf21530f9c9ee90f5571605bde74a
Wamea <30634582+Wamezach@users.noreply.github.com>
2025-10-06 08:24:50 +0800

    Update proxy-cover.js

--- api/proxy-cover.js
+++ api/proxy-cover.js
@@ -5,13 +5,12 @@ module.exports = async (req, res) => {
     if (!id || !filename) {
         return res.status(400).send('Missing id or filename');
     }
-
     const imageUrl = `https://uploads.mangadex.org/covers/${id}/${filename}`;
-
     try {
         const response = await axios.get(imageUrl, { responseType: 'stream' });
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Content-Type', response.headers['content-type']);
+        // Proxy the image stream directly
         response.data.pipe(res);
     } catch (err) {
         res.status(404).send('Cover not found');
